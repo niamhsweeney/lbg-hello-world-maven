f3943c6375f2a4b7043d78536730b225e5220f47
niamh <niamh@qa.com>
2024-01-16 12:45:41 +0000

    s added

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.println(sayHelloToSomeone("Z"));
+        System.out.println(sayHelloToSomeone("Zz"));
         system.out.printIn(sayHelloToSomeone("Niamhh"));
         System.out.println(sayGoodbye());
     }
